f2e9856a9867f6c704f1a8da37bf8cf89d8d99aa
harshilsfilm <147140889+harshilsfilm@users.noreply.github.com>
2025-12-18 23:48:05 +0530

    Add files via upload

---
+++ class_list.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="class_list.aspx.cs" Inherits="class_list.class_list" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Enter ID: <asp:TextBox ID="txtid" runat="server"></asp:TextBox>
+            <br />
+            Enter Name: <asp:TextBox ID="txtname" runat="server"></asp:TextBox>
+            <br />
+            Enter Salary: <asp:TextBox ID="txtsalary" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="Btn_sub" runat="server" Text="Submit" OnClick="Btn_sub_Click" />
+
+            <br />
+            <asp:Label ID="lstLabel" runat="server" Text=""></asp:Label>
+
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" ControlToValidate="txtname" runat="server" ErrorMessage="RequiredFieldValidator"></asp:RequiredFieldValidator>
+        </div>
+    </form>
+</body>
+</html>
---
+++ class_list.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace class_list
+{
+   
+
+    public partial class class_list : System.Web.UI.Page
+    {
+        public class Employee
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public int salary { get; set; }
+        }
+
+        List<Employee> emplist;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                emplist = new List<Employee>();
+                Session["emplist"] = emplist;
+            }
+            Page.UnobtrusiveValidationMode=UnobtrusiveValidationMode.None;
+        }
+
+        protected void Btn_sub_Click(object sender, EventArgs e)
+        {
+            emplist = (List<Employee>)Session["emplist"];
+            Employee emp=new Employee
+            {
+                ID = int.Parse(txtid.Text),
+                Name = txtname.Text,
+                salary=int.Parse(txtsalary.Text),
+            };
+
+            emplist.Add(emp);
+            //var res=emplist.ToList(); for without where , show all data
+
+            var result=emplist.Where(i => i.salary>30000).ToList();
+            foreach (var i in result)
+            {
+                lstLabel.Text += "ID:" + i.ID + "Name:" + i.Name + "Salary:" + i.salary;
+            }
+
+        }
+    }
+}
